b15f51450127f7ee996407831031a0a9d936e658
Keisuke05410 <keisuke3469@gmail.com>
2023-09-15 13:37:20 +0900

    Refactor VisiterHeader component and add authentication logic

--- app/components/visiterHeader.tsx
+++ app/components/visiterHeader.tsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-const VisiterHeader = () => {
-    return (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
-                <h1 className=" font-black text-2xl m-auto">AtPro</h1>
-            </div>
-            <div className="flex-none">
-                <a href="/login" className="btn btn-active btn-neutral m-auto">
-                    Login
-                </a>
-            </div>
-        </div>
-    );
-};
+export default async function VisiterHeader() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-export default VisiterHeader;
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <h1 className=" font-black text-2xl m-auto">AtPro</h1>
+      </div>
+      <div className="flex-none">
+        {user ? (
+          <a href="/dashboard" className="btn btn-active btn-neutral m-auto">
+            DashBoard
+          </a>
+        ) : (
+          <a href="/login" className="btn btn-active btn-neutral m-auto">
+            Login
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
